d53b8f56ee5da5a3c2f04145c197c044f4ebcd1c
Archit Parnami <architparnami@gmail.com>
2020-07-30 12:32:37 -0400

    select noise type

--- src/np_pixel.py
+++ src/np_pixel.py
@@ -2,22 +2,26 @@ import numpy as np
 from scale import Scale
 from noise import Noise
 
-def np_pixelate(img, target_h, target_w, sigma, 
+def np_pixelate(img, target_h, target_w, control, 
                 noise_factor = 1, 
-                scale_f = Scale.pad_and_pixelate):
+                scale_f = Scale.pad_and_pixelate,
+                noise_f = Noise.add_gaussian_noise):
     """
     Input:
         img: numpy array of your image
         target_h: required height of the pixelated image
         target_w: required width of the pixelated image
-        sigma: standard deviation for gaussian noise
+        control: standard deviation for gaussian noise or scale incase of laplacian noise
         noise_factor: scale the noise by this factor (default: 1 i.e., don't scale).
         scale_f: function for scaling the image to target_h, target_w
             Scale.with_pillow: Uses pillow image library. Scaled image is interpolated.
             Scale.crop_and_pixelate: crops boundary pixels and then performs pixelation.
             Scale.pad_and_pixelate: pads 0's to boundary pixels and then performs pixelation. (default) 
+        noise_f: function that adds noise to the image
+            Noise.add_gaussian_noise: add's laplace noise with loc=0 and scale=control
+            Noise.add_laplace_noise: add's gaussian noise with mean=0 and stdev=control
     Output:
-        Return: non private pixelated image with gaussian noise of dimension (target_h, target_w, input_channels)
+        Return: non private pixelated image with noise of dimension (target_h, target_w, input_channels)
     """
 
     flag = False
@@ -27,7 +31,7 @@ def np_pixelate(img, target_h, target_w, sigma,
 
    
     scaled_img = scale_f(img, target_h, target_w)
-    np_px_img = Noise.add_gaussian_noise(scaled_img, 0, sigma, noise_factor=noise_factor)
+    np_px_img = noise_f(scaled_img, 0, control, noise_factor=noise_factor)
    
     if flag:
         np_px_img = np.squeeze(np_px_img)
